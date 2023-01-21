407b108923130cbe0c8c9be099350f2e5271b47d
Lautaro Vilas <vilaslautaro@gmail.com>
2023-01-20 22:27:46 -0300

    updated test

--- src/tests/LetterBox.test.tsx
+++ src/tests/LetterBox.test.tsx
@@ -4,19 +4,16 @@ import { LetterBox } from '../components/rows/LetterBox.component'
 describe('test component letterbox', () => {
 	test('Check whether an existing letter is rendered as correct at the correct position in the solution', () => {
 		const { container } = render(<LetterBox value={'A'} status={'correct'} />)
-		const classNamesInput = container.firstChild?.className
-		expect(classNamesInput).toContain('bg-green')
+		expect(container.getElementsByClassName('bg-green')).toBeTruthy()
 	})
 
 	test('Check if an existing letter in the solution is rendered as present but in incorrect position', () => {
 		const { container } = render(<LetterBox value={'A'} status={'present'} />)
-		const classNamesInput = container.firstChild?.className
-		expect(classNamesInput).toContain('bg-yellow')
+		expect(container.getElementsByClassName('bg-yellow')).toBeTruthy()
 	})
 
 	test('Check if a letter that does not exist in the solution is rendered as absent', () => {
 		const { container } = render(<LetterBox value={'A'} status={'absent'} />)
-		const classNamesInput = container.firstChild?.className
-		expect(classNamesInput).toContain('bg-gray')
+		expect(container.getElementsByClassName('bg-gray')).toBeTruthy()
 	})
 })
